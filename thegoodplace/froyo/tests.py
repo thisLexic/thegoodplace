a6156bdf7d2deb580e69a3d2e6bbf896c30f23d2
thisLexic <benvillabroza1999@gmail.com>
2020-03-18 15:46:55 +0800

    defined the setUp and tearDown function in tests.py of the froyo app

--- thegoodplace/froyo/tests.py
+++ thegoodplace/froyo/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import unittest
+from django.urls import resolve
+from selenium import webdriver
 
-# Create your tests here.
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
