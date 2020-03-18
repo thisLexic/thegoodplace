d1c6dc30c0878c11fca3036e255893355968f3e3
thisLexic <benvillabroza1999@gmail.com>
2020-03-18 15:26:01 +0800

    added urls for all views but only connected it to ingredients_list view initially

--- thegoodplace/froyo/urls.py
+++ thegoodplace/froyo/urls.py
@@ -2,5 +2,18 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'ingredients_list/', views.ingredients_list)
+    url(r'ingredients_list/', views.ingredients_list),
+    url(r'ingredients_detail/', views.ingredients_list),
+    url(r'ingredients_update_form/', views.ingredients_list),
+    url(r'ingredients_create_form/', views.ingredients_list),
+
+    url(r'recipes_list/', views.ingredients_list),
+    url(r'recipes_detail/', views.ingredients_list),
+    url(r'recipes_update_form/', views.ingredients_list),
+    url(r'recipes_create_form/', views.ingredients_list),
+
+    url(r'orders_list/', views.ingredients_list),
+    url(r'orders_detail/', views.ingredients_list),
+    url(r'orders_update_form/', views.ingredients_list),
+    url(r'orders_create_form/', views.ingredients_list),
 ]
